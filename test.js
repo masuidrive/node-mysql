82455b4e6ce8ffac5bcce74a80635d0dbbc06ef0
Yuichiro MASUI <masuidrive@masuidrive-mbp.local>
2010-02-13 05:39:02 -0800

    clean up and more error handling

--- test.js
+++ test.js
@@ -1,17 +1,17 @@
 var sys = require('sys');
 var mysql = require('./lib/mysql');
 
-var conn = new mysql.createConnection('localhost','nodejs_mysql', 'nodejs_mysql', 'nodejs_mysql');
-setTimeout(function() {
-    conn.query("SELECT now();")
-	.addCallback(function(n) {
-	    sys.puts("!");
-	})
-	.addErrback(function(type, message) {
-	    sys.puts("Error: "+type+" "+message);
-	});
-//sys.puts(sys.inspect(conn.query_command("select now() as N;")));
-}, 1000);
+var conn = new mysql.Connection();
+conn.connect('localhost','nodejs_mysql', 'nodejs_mysql', 'nodejs_mysql')
+    .addCallback(function() {
+	conn.query("SELECT now();")
+	    .addCallback(function(n) {
+		sys.puts("!");
+	    })
+	    .addErrback(function(type, message) {
+		sys.puts("Error: "+type+" "+message);
+	    });
+    });
 
 /*
   CREATE DATABASE nodejs_mysql;

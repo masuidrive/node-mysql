62c695e73f00d6cd4cc02bb194c074df51527bc4
masuidrive <masui@masuidrive.jp>
2010-02-14 07:22:33 -0800

    fix to return null

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ conn.connect('localhost','nodejs_mysql', 'nodejs_mysql', 'nodejs_mysql')
     .addCallback(function() {
 	 //conn.query("SELECT now();")
 	//conn.query("SELECT * FROM test1;")
-	conn.query("SELECT 1,2.3,'abc';")
+	conn.query("SELECT 1,2.3,'abc',true,null,'val' as value;")
 	    .addCallback(function(res) {
 		for(var i=0; i<res.length; ++i) {
 		    sys.puts("Result: "+sys.inspect(res[i].toHash()));

450c4e4cd30404252fbc4e9c42a303725587751a
Yuichiro MASUI <masuidrive@masuidrive-mbp.local>
2010-02-16 01:37:18 -0800

    fix no-return query ex)create table

--- test.js
+++ test.js
@@ -2,19 +2,18 @@ var sys = require('sys');
 var mysql = require('./lib/mysql');
 
 var conn = new mysql.Connection();
-conn.connect('localhost','nodejs_mysql', 'nodejs_mysql', 'nodejs_mysql')
-    .addCallback(function() {
-	 conn.query("SELECT now();")
-	//conn.query("SELECT * FROM test1;")
-	conn.query("SELECT 1,2.3,'abc',true,null,'val' as value;")
-	    .addCallback(function(res) {
-		for(var i=0; i<res.length; ++i) {
-		    sys.puts("Result: "+sys.inspect(res[i].toHash()));
-		};
-	    })
-	    .addErrback(function(type, message) {
-		sys.puts("Error: "+type+" "+message);
-	    });
+
+conn.connect('localhost','nodejs_mysql', 'nodejs_mysql', 'nodejs_mysql');
+conn.query("CREATE TEMPORARY TABLE test1(intval INTEGER, strval TEXT, timestampval TIMESTAMP, boolval BOOLEAN);");
+conn.query("INSERT INTO test1 VALUES(1,'a',now(),true);")
+conn.query("SELECT * FROM test1;")
+    .addCallback(function(res) {
+	for(var i=0; i<res.length; ++i) {
+	    sys.puts("Result: "+sys.inspect(res[i].toHash()));
+	};
+    })
+    .addErrback(function(type, message) {
+	sys.puts("Error: "+type+" "+message);
     });
 
 /*

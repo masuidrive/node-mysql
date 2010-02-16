92fe1dc441ff51ebab5dde81af7aaf832de88c88
masuidrive <masui@masuidrive.jp>
2010-02-16 04:46:17 -0800

    Update readme

--- example.js
+++ example.js
@@ -9,7 +9,7 @@ GRANT ALL ON nodejs_mysql.* TO nodejs_mysql@localhost IDENTIFIED BY "nodejs_mysq
 var conn = new mysql.Connection();
 conn.connect('localhost','nodejs_mysql', 'nodejs_mysql', 'nodejs_mysql');
 conn.query("CREATE TEMPORARY TABLE test1(intval INTEGER, strval TEXT, timestampval TIMESTAMP, boolval BOOLEAN);");
-conn.query("INSERT INTO test1 VALUES(1,'a',now(),true);")
+conn.query("INSERT INTO test1 VALUES(1,'a',now(),true);");
 conn.query("SELECT * FROM test1;")
     .addCallback(function(result) {
 	for(var i=0; i<result.records.length; ++i) {
